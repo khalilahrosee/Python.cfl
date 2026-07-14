b5e212b89621b943937561c62d4a0d34c025ad44
khalilahrosee <Kdiakite523@icloud.com>
2026-07-14 16:18:37 -0400

    Add files via upload

---
+++ conditionalsbutmore.py
@@ -0,0 +1,11 @@
+fighter = "warrior"
+key = True
+energy = 35
+
+if fighter == "warrior" and key == True:
+    print("Congrats, you can open the door!")
+    if energy > 30:
+        print("You can open the door, energy is sufficient!")
+if
+
+
