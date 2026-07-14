af94bd5369d8206e812fc0f62f6f36095d4d6d25
khalilahrosee <Kdiakite523@icloud.com>
2026-07-13 21:40:39 -0400

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,55 @@
+print("*****Chat with Python!*****")
+name = input("Hi!,I'm Python, what's your name?")
+print("Hey", name, "")
+emotion = input("How are you doing today, " + name + ", happy, sad, excited, or mad?")
+if emotion == "happy":
+    print("Im happy you're happy :]")
+elif emotion == "sad":
+    print("Im sorry to hear that :[")
+elif emotion == "excited":
+    print("Seems you got a ton of energy!")
+elif emotion == "mad":
+    print("I hope i'm not the problem...:/")
+
+coding = input("Wait a minute! Are you a coder :0??, yes or no?")
+if coding == "yes":
+    print("Yayy, thats so cool! I wish i could code too :[")
+elif coding == "no":
+    print("Well you should code >:[")
+Game = input("Lets play rock paper scissors!, rock, paper, or scissors?")
+if Game == "rock":
+    print("I choose scissors, you win!")
+elif Game == "paper":
+    print("I choose scissors, I win!")
+elif Game == "scissors":
+    print("I choose scissors too, its a tie!")
+color= input("Whats your favorite color?")
+if color == "red":
+    print("Red is a great color!")
+elif color == "blue":
+    print("Blue is a great color!")
+elif color == "green":
+    print("Green is a great color!")
+elif color == "yellow":
+    print("Yellow is a great color!")
+elif color == "purple":
+    print("Purple is a great color!")
+elif color == "rainbow":
+    print("Rainbow? thats all the colors, you must be a very colorful person!")
+else:
+    print("Cool, you have good taste!")
+
+Videogames = input("Do you like video games? yes or no?")
+
+if Videogames == "yes":
+    print("Yayy, me too!")
+elif Videogames == "no":
+    print("Well you sound boring, but thats ok :[")
+
+goodbye = input("Well, it was nice talking to you, " + name + ", goodbye!")
+if goodbye == "bye":
+    print("Farewell!")
+else:
+    print("Just say goodbye like a normal person gng..")
+
+
