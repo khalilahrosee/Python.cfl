b10e2db15f04c7730a2598533de1a8a7da8bacfd
khalilahrosee <Kdiakite523@icloud.com>
2026-07-15 16:25:36 -0400

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,11 @@
+import random
+
+words = [Happy, Earth, Space]
+random_word = random.choice(words)
+letters_guessed = []
+lives = 6
+def display_word():
+    return
+
+def guessed_word():
+    return
